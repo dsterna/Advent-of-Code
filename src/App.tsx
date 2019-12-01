243755c72040c19bd274fc72e80ab3438c120b66
dsterna <dsterna@kth.se>
2019-12-01 21:27:09 +0100

    two stars

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './input/1.json'
+import { number } from 'prop-types';
+import { endianness } from 'os';
+
+
+/* OK */
 
 const App: React.FC = () => {
+  function foo(v: string) { return 'foo'; }
+  function hejsan(num: number) {
+    return Math.floor(num / 3) - 2;
+
+  }
+  let sum = 0;
+  data.map(num => {
+    let hej = hejsan(num);
+    sum+=hej;
+    while (hej > 0) {
+      hej = hejsan(hej)
+      if(hej > 0){
+        sum += hej;
+        console.log("num: ", num, "calc: ", hej, "sum: ", sum )
+      }
+      
+    }
+  })
+  console.log('sum', sum);
+  // data.map(num => {
+  //   num = 1969;
+  //   let hej =1;
+  //   while(hej > 0){
+  //     let hej =hejsan(num);
+  //     sum+=hej;
+  //     console.log("num: ", num, "calc: ", hej, "sum: ", sum )
+  //   }
+  //   return;
+
+  // })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
