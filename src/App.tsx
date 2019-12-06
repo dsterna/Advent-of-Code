d4aee3714ad9793ac2a034292c051129f3088c61
dsterna <dsterna@kth.se>
2019-12-06 15:01:30 +0100

    i am useless

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,60 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import _ from 'lodash';
+import React from 'react';
+import './App.css';
+import initialData from './input/1.json';
+/**
+|--------------------------------------------------
+- D directly orbits C and indirectly orbits B and COM, a total of 3 orbits.
+- L directly orbits K and indirectly orbits J, E, D, C, B, and COM, a total of 7 orbits.
+- COM orbits nothing.
 
-/**
+|         G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I
 |--------------------------------------------------
-| It is a six-digit number.
-  The value is within the range given in your puzzle input.
-  Two adjacent digits are the same (like 22 in 122345).
-  Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
-|--------------------------------------------------
+
+B)  C       c->B +1 nya tar förälderns relationer  D->C+1 (+1)
+C)  D
+D)  E
+E)  F
+B)  G
+G)  H
+D)  I
+E)  J
+J)  K
+K)  L
+
 */
-// RANGE IS 137683-596253
-/**
-|--------------------------------------------------
-| 111111 meets these criteria (double 11, never decreases).
-  223450 does not meet these criteria (decreasing pair of digits 50).
-  123789 does not meet these criteria (no double).
-|--------------------------------------------------
-*/
+type orbit =  {
+  name: string
+  count: number
+}
+
+export interface IArray extends Array<orbit> { }
+
 const App: React.FC = () => {
-  const digitArray = (rangeNumber: number): number[] => {
-    return ("" + rangeNumber).split("").map(function (t) { return parseInt(t) })
-  }
-  const countAdjacent = (ary: number[], position: number): boolean => {
-    let count = 0;
-    let digit = ary[position];
-    for (let index = position + 1; index < ary.length; index++) {
-      if (ary[index] === digit) {
-        count++;
-      }
-      else
-        break;
-    }
-    for (let index = position - 1; index >= 0; index--) {
-      if (ary[index] === digit) {
-        count++;
-      }
-      else
-        break;
-    }
-    if ((count + 1) === 2) {
-      return true;
-    }
-    return false;
-  }
+  let data = [...initialData];
+  console.log('data', data);
+  let orbits:IArray = data.map(elem => {return {name: elem.split(')')[0], count: 0}});
 
-  const startInt: number = 137683;
-  const endInt: number = 596253;
-  let posiblePasswords: any[] = [];
-  for (let nextIndex = 137683; nextIndex < endInt; nextIndex++) {
-    let foundAdjecent = false;
-    let neverDecrease = true;
-    let testInt = nextIndex + 1;
-    let ary: number[] = digitArray(testInt);
-    ary.forEach((digit, index) => {
-      if (index >= ary.length - 1) {
-      }
-      if (ary[index + 1] < digit) {
-        neverDecrease = false;
-        return
-      }
-      if (ary[index + 1] == digit) {
-        if (countAdjacent(ary, index)) {
-          foundAdjecent = true;
-        };
-      }
-    })
-    if (foundAdjecent && neverDecrease) {
-      posiblePasswords.push(testInt)
-    }
-  }
-  console.log('array', posiblePasswords);
+console.log(orbits);
+
+  data.forEach((elem, index) => {
+    let first = elem.split(')')[0];
+    let sec = elem.split(')')[1];
+    console.log('sec', sec);
+
+   console.log(
+     "hej", orbits.find(elem => (sec === elem.name )));
+
+  })
+
+
+
   return (
     <div className="App">
     </div>
   );
-};
+}
 export default App;
